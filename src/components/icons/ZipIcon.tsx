d0ecb04c1d6c7e125bdddad3ad2545012992cf90
Radka Filipová <r.filipova@email.cz>
2026-01-09 09:26:54 +0100

    Replace ZipIcon SVG with a new icon

--- src/components/icons/ZipIcon.tsx
+++ src/components/icons/ZipIcon.tsx
@@ -2,9 +2,9 @@
 import React from 'react';
 
 const ZipIcon: React.FC = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
-        <path strokeLinecap="round" strokeLinejoin="round" d="M20 12H4" />
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-download-icon lucide-download"><path d="M12 15V3"/>
+        <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
+        <path d="m7 10 5 5 5-5"/>
     </svg>
 );
 
