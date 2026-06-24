f8d3703170f6208ca542241bf63db7d6e9b768c0
Ra-Fil <r.filipova@email.cz>
2026-06-24 20:46:59 +0200

    add test smtp

--- server.js
+++ server.js
@@ -413,6 +413,8 @@ app.get('/api/test-smtp', async (req, res) => {
         secure: config.secure,
         name: process.env.SMTP_EHLO,
         auth: { user: process.env.SMTP_USER, pass: process.env.SMTP_PASS },
+        connectionTimeout: 10000,
+        greetingTimeout: 10000,
     });
 
     try {
